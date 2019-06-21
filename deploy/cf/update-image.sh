b789c5c26a4ca2c1698399be018d3fed8a3dc78f
tools4assessment <51959922+tools4assessment@users.noreply.github.com>
2019-06-21 21:14:10 +0800

    Revert "Update update-image.sh"
    This reverts commit f622bac3efb2f98826afaf5f4f429fd4d48c2186.

--- deploy/cf/update-image.sh
+++ deploy/cf/update-image.sh
@@ -1,21 +1,19 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 set -x
 opwd=$(pwd)
 cd /home/concerto
-oid=$(docker images | grep 'tools4assessment/concerto-v5.*master' | awk '{print $3}')
-cid=$(docker container ls -a | grep 'tools4assessment/concerto-v5.*master' | awk '{print $1}')
-out=$(docker pull tools4assessment/concerto-v5:master)
+oid=$(docker images | grep 'tools4assessment/concerto-v5.*latest' | awk '{print $3}')
+out=$(docker pull tools4assessment/concerto-v5:latest)
 opwd=$(pwd)
 if [[ $out != *"Image is up to date"* ]];
 then
   echo "re compose, delete old image"
-  docker stop $cid
-  docker container rm $cid
+  docker-compose down
   docker rmi $oid
-  docker-compose up -d concerto
+  docker-compose up
 else
   echo "up to date, no action"
-  docker inspect --format "{{.Id}}" $oid
+  docker inspect --format "{{.Id}}"  $oid
 fi
 cd $opwd
 exit 0
